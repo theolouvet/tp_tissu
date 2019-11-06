3f1261c9c71fe9fd994d64b2cb15d1701d1f2a4e
Theo Louvet <theo.louvet@mastertp>
2019-11-06 13:57:15 +0100

    init

---
+++ tp_tissus_partie_2/project/src/cloth/mesh_parametric_cloth.cpp
@@ -0,0 +1,163 @@
+/*
+**    TP CPE Lyon
+**    Copyright (C) 2015 Damien Rohmer
+**
+**    This program is free software: you can redistribute it and/or modify
+**    it under the terms of the GNU General Public License as published by
+**    the Free Software Foundation, either version 3 of the License, or
+**    (at your option) any later version.
+**
+**   This program is distributed in the hope that it will be useful,
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**    GNU General Public License for more details.
+**
+**    You should have received a copy of the GNU General Public License
+**    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "mesh_parametric_cloth.hpp"
+
+#include "../lib/common/error_handling.hpp"
+#include <cmath>
+
+namespace cpe
+{
+
+
+void mesh_parametric_cloth::update_force()
+{
+
+    int const Nu = size_u();
+    int const Nv = size_v();
+    int const N_total = Nu*Nv;
+    ASSERT_CPE(static_cast<int>(force_data.size()) == Nu*Nv , "Error of size");
+
+
+    //Gravity
+    static vec3 const g (0.0f,0.0f,-9.81f);
+    vec3 const g_normalized = g/N_total;
+    for(int ku=0 ; ku<Nu ; ++ku)
+    {
+        for(int kv=0 ; kv<Nv ; ++kv)
+        {
+            force(ku,kv) = g_normalized;
+        }
+    }
+
+    //*************************************************************//
+    // TO DO, Calculer les forces s'appliquant sur chaque sommet
+    //*************************************************************//
+    //
+    // ...
+    //
+    //
+    //
+    //*************************************************************//
+
+
+}
+
+void mesh_parametric_cloth::integration_step(float const dt)
+{
+    ASSERT_CPE(speed_data.size() == force_data.size(),"Incorrect size");
+    ASSERT_CPE(static_cast<int>(speed_data.size()) == size_vertex(),"Incorrect size");
+
+
+    int const Nu = size_u();
+    int const Nv = size_v();
+    //*************************************************************//
+    // TO DO: Calculer l'integration numerique des positions au cours de l'intervalle de temps dt.
+    //*************************************************************//
+    //
+    // ...
+    //
+    //
+    //
+    //*************************************************************//
+
+
+    //security check (throw exception if divergence is detected)
+    static float const LIMIT=30.0f;
+    for(int ku=0 ; ku<Nu ; ++ku)
+    {
+        for(int kv=0 ; kv<Nv ; ++kv)
+        {
+            vec3 const& p = vertex(ku,kv);
+
+            if( norm(p) > LIMIT )
+            {
+                throw exception_divergence("Divergence of the system",EXCEPTION_PARAMETERS_CPE);
+            }
+        }
+    }
+
+}
+
+void mesh_parametric_cloth::set_plane_xy_unit(int const size_u_param,int const size_v_param)
+{
+    mesh_parametric::set_plane_xy_unit(size_u_param,size_v_param);
+
+    int const N = size_u()*size_v();
+    speed_data.resize(N);
+    force_data.resize(N);
+}
+
+vec3 const& mesh_parametric_cloth::speed(int const ku,int const kv) const
+{
+    ASSERT_CPE(ku >= 0 , "Value ku ("+std::to_string(ku)+") should be >=0 ");
+    ASSERT_CPE(ku < size_u() , "Value ku ("+std::to_string(ku)+") should be < size_u ("+std::to_string(size_u())+")");
+    ASSERT_CPE(kv >= 0 , "Value kv ("+std::to_string(kv)+") should be >=0 ");
+    ASSERT_CPE(kv < size_v() , "Value kv ("+std::to_string(kv)+") should be < size_v ("+std::to_string(size_v())+")");
+
+    int const offset = ku + size_u()*kv;
+
+    ASSERT_CPE(offset < static_cast<int>(speed_data.size()),"Internal error");
+
+    return speed_data[offset];
+}
+
+vec3& mesh_parametric_cloth::speed(int const ku,int const kv)
+{
+    ASSERT_CPE(ku >= 0 , "Value ku ("+std::to_string(ku)+") should be >=0 ");
+    ASSERT_CPE(ku < size_u() , "Value ku ("+std::to_string(ku)+") should be < size_u ("+std::to_string(size_u())+")");
+    ASSERT_CPE(kv >= 0 , "Value kv ("+std::to_string(kv)+") should be >=0 ");
+    ASSERT_CPE(kv < size_v() , "Value kv ("+std::to_string(kv)+") should be < size_v ("+std::to_string(size_v())+")");
+
+    int const offset = ku + size_u()*kv;
+
+    ASSERT_CPE(offset < static_cast<int>(speed_data.size()),"Internal error");
+
+    return speed_data[offset];
+}
+
+vec3 const& mesh_parametric_cloth::force(int const ku,int const kv) const
+{
+    ASSERT_CPE(ku >= 0 , "Value ku ("+std::to_string(ku)+") should be >=0 ");
+    ASSERT_CPE(ku < size_u() , "Value ku ("+std::to_string(ku)+") should be < size_u ("+std::to_string(size_u())+")");
+    ASSERT_CPE(kv >= 0 , "Value kv ("+std::to_string(kv)+") should be >=0 ");
+    ASSERT_CPE(kv < size_v() , "Value kv ("+std::to_string(kv)+") should be < size_v ("+std::to_string(size_v())+")");
+
+    int const offset = ku + size_u()*kv;
+
+    ASSERT_CPE(offset < static_cast<int>(force_data.size()),"Internal error");
+
+    return force_data[offset];
+}
+
+vec3& mesh_parametric_cloth::force(int const ku,int const kv)
+{
+    ASSERT_CPE(ku >= 0 , "Value ku ("+std::to_string(ku)+") should be >=0 ");
+    ASSERT_CPE(ku < size_u() , "Value ku ("+std::to_string(ku)+") should be < size_u ("+std::to_string(size_u())+")");
+    ASSERT_CPE(kv >= 0 , "Value kv ("+std::to_string(kv)+") should be >=0 ");
+    ASSERT_CPE(kv < size_v() , "Value kv ("+std::to_string(kv)+") should be < size_v ("+std::to_string(size_v())+")");
+
+    int const offset = ku + size_u()*kv;
+
+    ASSERT_CPE(offset < static_cast<int>(force_data.size()),"Internal error");
+
+    return force_data[offset];
+}
+
+
+}
